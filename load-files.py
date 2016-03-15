7b3e4c359df9e1c887060e1781e9c599014f3464
shiron <p.stashevsky@2gis.ru>
2016-03-15 16:43:42 +0600

    parsing logic

--- load-files.py
+++ load-files.py
@@ -1,25 +1,29 @@
-# -*- coding: utf-8 -*-
+#coding=utf-8
+from __future__ import unicode_literals
 from astropy.utils.compat.futures import ThreadPoolExecutor
 from textparsing import tokenize
 import os
 from collections import Counter
 
 
-DATA_DIR = './text_files/'
-COUNT_WORKERS = 4
+DATA_DIR = './test/'
+COUNT_WORKERS = 1
 
 def parse(file):
-    tokens = tokenize(open(DATA_DIR + file).read())
-    tokens_new = []
-    for token in tokens:
-        tokens_new.append(token.decode('utf-8'))
+    #open text file and get tokens
+    with open(DATA_DIR + file) as text_file:
+        is_fraud, tokens = tokenize(text_file.read())
 
-    count = Counter(tokens_new)
-    print(count.most_common(5))
+    #calculate count of tokens
+    count = Counter(tokens)
+    mc = count.most_common(5)
+    for key,value in mc:
+        print key, value
+    #write to db
 
 
-with ThreadPoolExecutor(max_workers=COUNT_WORKERS) as executor:
-    #get file list
-    file_list = os.listdir(DATA_DIR)
-    for file in file_list:
-        executor.submit(parse, file)
+if __name__ == "__main__":
+    with ThreadPoolExecutor(max_workers=COUNT_WORKERS) as executor:
+        file_list = os.listdir(DATA_DIR)
+        for file in file_list:
+            executor.submit(parse, file)
